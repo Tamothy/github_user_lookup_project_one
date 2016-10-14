7dc46e9682d5bbdb0639c0def735cef86683c941
Tam Nguyen <mr.tamron@gmail.com>
2016-10-14 11:33:11 -0700

    add pic/username/fullname properties in back-end-interface.js

--- build/js/app.js
+++ build/js/app.js
@@ -10,7 +10,12 @@ function GithubExport() {
 GithubExport.prototype.getRepos = function(nameInput){
   $.get('https://api.github.com/users/' + nameInput + '?access_token=' + apiKey).then(function(response){
     debugger;
-    console.log(response);
+    $('#gh-user-pic').empty();
+    $('#gh-user-pic').append('<img src=' + response.avatar_url + '>');
+    $('#gh-username').empty();
+    $('#gh-username').append('<h3 class="login">Username: <span class="style">' + response.login + '</span></h3>');
+    $('#gh-fullname').empty();
+    $('#gh-fullname').append('<h3 class="name">Name: <span class="style">' + response.name + '</span></h3>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
